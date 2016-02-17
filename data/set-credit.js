7dfdd8789e0fa044c765601e6ad6a3bebd6c2fcf
Subhasis Maity <iam.sm.work@gmail.com>
2016-02-17 14:01:13 -0800

    ready to deploy

---
+++ data/set-credit.js
@@ -0,0 +1,20 @@
+var fileName = 'ce2';
+
+var anskey = require('./' + fileName + '.json'),
+    _ = require('underscore'),
+    fs = require('fs'),
+    credit1 = require('./credit1/credit1-' + fileName);
+
+_.each(anskey, function (val, key){
+    if (credit1[key]) {
+        anskey[key]['credit'] = 1;
+    }else{
+        anskey[key]['credit'] = 2;
+    }
+})
+
+fs.writeFile(fileName + '.json', JSON.stringify(anskey), function(err){
+    if (err) {
+        console.log(err);
+    }
+})
